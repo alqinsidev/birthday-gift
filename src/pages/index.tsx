d7bc8afa6224357278432da6d1751724c82a131b
alqinsidev <padlanalqinsi@live.com>
2023-08-26 05:55:07 +0700

    hotfix: fix build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { useSpring, animated, useChain, useSpringRef } from '@react-spring/web';
+// @ts-ignore
 import useSound from 'use-sound'
 import Head from 'next/head';
 import React, { useState } from 'react';
 import { isBeforeAugust28GMT7 } from '@/helpers/date';
+import Image from 'next/image'
 
 const Home = () => {
   const [isVisible, setIsVisible] = useState(false)
@@ -18,7 +20,7 @@ const Home = () => {
     to: { opacity: 1 }, // Fade in when isVisible is false
     config: { duration: 1000 },
   });
-  
+
   // Animation for the h2 element when fading out
   const fadeOut = useSpring({
     ref: fadeOutRef,
@@ -43,23 +45,25 @@ const Home = () => {
   return (
     <div className='min-h-screen'>
       <Head>
-        <title>HBD Nisa 🎂</title>
+        <title>A letter to Nisa 🎂</title>
         <meta property='og:title' content='HBD Nisa 🎂' key='title' />
       </Head>
 
-      <animated.div className={'min-h-screen flex items-center justify-center flex-col'}>
-        <animated.h2 className='text-2xl mb-4 font-mono' style={isVisible ? fadeOut : fadeIn}>
-          A letter to Nisa
-        </animated.h2>
+      <animated.div className={'min-h-screen flex items-center justify-center flex-col bg-orange-50'}>
+        <animated.div className={'flex items-center justify-center flex-col'} style={fadeIn}>
+          <animated.h2 className='text-2xl mb-4 font-mono'>
+            A letter to Nisa
+          </animated.h2>
+        </animated.div>
         <animated.button
           className={`bg-blue-500 hover:bg-blue-700 rounded text-white p-2`}
           style={buttonSpring}
           disabled={!isBeforeAugust28GMT7()}
           onClick={() => {
-            isBeforeAugust28GMT7() ? null: play()
+            isBeforeAugust28GMT7() ? null : play()
           }}
         >
-          {isBeforeAugust28GMT7() ? 'PESAN BELUM BISA DIBUKA' : 'BUKA PESAN'}
+          {isBeforeAugust28GMT7() ? 'BELUM WAKTUNYA HEY' : 'BUKA PESAN'}
         </animated.button>
       </animated.div>
     </div>
